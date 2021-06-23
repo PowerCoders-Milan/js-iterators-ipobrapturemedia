caffcede98a5574dca9aff60e9f89f1d324ecfc3
Jackson Moses <ipobrapturemedia@gmail.com>
2021-06-23 12:09:12 +0200

    Assignment 3 done

--- 03-filter/filter.js
+++ 03-filter/filter.js
@@ -1,7 +1,18 @@
 // create a randomNumbers array, containing 100 random numbers
-
+const randomNumbers = [];
+for (let i = 0; i < 100; i++) {
+     randomNumbers.push(Math.ceil(Math.random()*200))
+}
 // create a function isPrime(number)
-
+const isPrime = Number => {
+    for (let i = 2; i < Number; i++){
+        if (Number % i === 0){
+            return false;
+        }
+        console.log (isPrime);
+    }
+   
 // create a primeNumbers array
 const primeNumbers = // filter randomNumbers with isPrime(number)
-console.log(primeNumbers)
+ console.log(primeNumbers)
+}
