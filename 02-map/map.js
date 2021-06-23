0b9e549f79fedf4e78cd44a9268d9c766839f39b
Jackson Moses <ipobrapturemedia@gmail.com>
2021-06-23 10:49:34 +0200

    Assignment 2 done

--- 02-map/map.js
+++ 02-map/map.js
@@ -1,13 +1,14 @@
-const animals = ['Hen', 'elephant', 'llama', 'leopard', 'ostrich', 'Whale', 'octopus', 'rabbit', 'lion', 'dog'];
-
-const secretMessage = // use .map to get the first letter of every animal name in the arrray animals
-
-console.log(secretMessage.join(''));
+const animals = ['Hen', 'elephant', 'llama', 'leopard','ostrich', 'Whale', 'octopus', 'rabbit', 'lion', 'dog'];
+//const secretMessage = // use .map to get the first letter of every animal name in the arrray animals
+const secretMessage = animals.map(firstLetter => {
+    return firstLetter[0]
+  });
+  console.log(secretMessage.join(''));
 
 
 const bigNumbers = [100, 200, 300, 400, 500];
 
-// Create the smallNumbers array below with map
+//Create the smallNumbers array below with map
 
-
+const smallNumbers = bigNumbers.map(number => number/10);
 console.log(smallNumbers);
